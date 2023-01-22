f6bd94d4988f8911e6be5f600ee2c2f5e3e05796
akiraah <rah24.@gmail.com>
2023-01-22 11:39:54 +0000

    init commit

---
+++ sls-notes-api/serverless.ts
@@ -0,0 +1,117 @@
+import type { AWS } from '@serverless/typescript'
+
+const serverlessConfiguration: AWS = {
+  service: 'sls-notes-api',
+  frameworkVersion: '3',
+  plugins: ['serverless-esbuild'],
+  provider: {
+    name: 'aws',
+    region: 'eu-west-2',
+    runtime: 'nodejs14.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      NOTES_TABLE: 'notes-table-${opt:stage, "dev"}',
+    },
+    iam: {
+      role: {
+        statements: [
+          {
+            Effect: 'Allow',
+            Action: 'dynamodb:*',
+            Resource:
+              'arn:aws:dynamodb:${opt:region, self:provider.region}:*:table/${self:provider.environment.NOTES_TABLE}',
+          },
+        ],
+      },
+    },
+  },
+  // import the function via paths
+  functions: {
+    createNote: {
+      handler: 'src/handlers/handler.createNote',
+      events: [
+        {
+          http: {
+            method: 'post',
+            path: 'note',
+          },
+        },
+      ],
+    },
+  },
+  package: { individually: true },
+  custom: {
+    esbuild: {
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      exclude: ['aws-sdk'],
+      target: 'node14',
+      define: { 'require.resolve': undefined },
+      platform: 'node',
+      concurrency: 10,
+    },
+  },
+  resources: {
+    Resources: {
+      NotesTable: {
+        Type: 'AWS::DynamoDB::Table',
+        DeletionPolicy: 'Retain',
+        Properties: {
+          TableName: '${self:provider.environment.NOTES_TABLE}',
+          AttributeDefinitions: [
+            {
+              AttributeName: 'user_id',
+              AttributeType: 'S',
+            },
+            {
+              AttributeName: 'timestamp',
+              AttributeType: 'N',
+            },
+            // {
+            //   AttributeName: 'note_id',
+            //   AttributeType: 'S',
+            // },
+          ],
+          KeySchema: [
+            {
+              AttributeName: 'user_id',
+              KeyType: 'HASH',
+            },
+            {
+              AttributeName: 'timestamp',
+              KeyType: 'RANGE',
+            },
+          ],
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 1,
+            WriteCapacityUnits: 1,
+          },
+          // GlobalSecondaryIndexes: [
+          //   {
+          //     IndexName: 'note_id-index',
+          //     KeySchema:  {
+          //       AttributeName: 'note_id',
+          //       KeyType: 'HASH',
+          //     },
+          //     Projection: {
+          //       ProjectionType: 'ALL',
+          //     },
+          //     ProvisionedThroughput: {
+          //       ReadCapacityUnits: 1,
+          //       WriteCapacityUnits: 1,
+          //     },
+          //   },
+          // ],
+        },
+      },
+    },
+  },
+}
+
+module.exports = serverlessConfiguration
---
+++ sls-notes-api/src/handlers/handler.ts
@@ -0,0 +1,42 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as AWS from 'aws-sdk'
+
+const client = new AWS.DynamoDB.DocumentClient()
+
+interface Note {
+  user_id: string
+  timestamp: number
+}
+export const createNote = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  console.log(`createNote lambda invoked with: ${JSON.stringify(event)}`)
+
+  const item: Note = {
+    user_id: 'test',
+    timestamp: new Date().getTime(),
+  }
+
+  const params = {
+    TableName: 'notes-table-dev',
+    Item: item,
+  }
+
+  try {
+    await client.put(params).promise()
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'note created successfully',
+      }),
+    }
+  } catch (error) {
+    console.log(`Error ${error.message}`)
+    return {
+      statusCode: 404,
+      body: JSON.stringify({
+        message: 'failed to create note',
+      }),
+    }
+  }
+}
